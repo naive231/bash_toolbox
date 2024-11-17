0149016ebb58c77ceb4776664cb7fb9bb9ced6c8
naive231 <naive231@gmail.com>
2024-11-17 09:43:47 +0800

    [delete] Remove the 'media_post_process.js', it doesn't work better than Python version or BASH verion. [add] Stage the 'm3u8_download.sh' to keep developing.

---
+++ m3u8_downloader.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+URL="$1"
+BASE_URL=$(echo "$URL" | sed -E 's|(https?://[^/]+).*|\1|')
+
+curl -s "$URL" | grep -Eo '(["'\''])([^"'\'' ]+\.m3u8[^"'\'' ]*)\1' | sed 's/^["'\'']//;s/["'\'']$//' | while read -r LINK; do
+    if [[ $LINK == http* ]]; then
+        echo "$LINK"
+    else
+        # Resolve relative URLs
+        if [[ $LINK == /* ]]; then
+            # Absolute path on the same domain
+            echo "$BASE_URL$LINK"
+        else
+            # Relative path
+            DIR_URL=$(dirname "$URL")
+            echo "$DIR_URL/$LINK"
+        fi
+    fi
+done | sort -u
